0e5ea725bdd519422cc82507ac61ed6dbffaa994
Guilherme H. Jardim <49923210+ghjardim@users.noreply.github.com>
2024-05-15 16:23:34 -0300

    Put preprocessing in a function

---
+++ main.py
@@ -0,0 +1,11 @@
+# from pathlib import Path
+from preprocessing import preprocess
+
+# pathlist = Path("data/20_newsgroups").glob('**/*')
+# for path in pathlist:
+#     path_in_str = str(path)
+#     print(path_in_str)
+
+path_in_str = "data/20_newsgroups/rec.sport.baseball/104501"
+data = preprocess(path_in_str)
+print(data)
--- preprocessing.py
+++ preprocessing.py
@@ -3,30 +3,30 @@ from nltk.corpus import stopwords
 import numpy as np
 from nltk.stem.snowball import SnowballStemmer
 
-filename="./data/20_newsgroups/alt.atheism/51128"
-f = open(filename, "r")
-data = ""
+def preprocess(filename):
+    f = open(filename, "r")
+    data = ""
 
-# Data reading and basic preprocessing
-for line in f.readlines():
-    line = line.rstrip()    # Removal of trailing '\n'
-    line = line.lstrip(">") # Removal of trailing '>' quotation symbol
-    data += line + " "
-data = data.lower() # To lowercase
+    # Data reading and basic preprocessing
+    for line in f.readlines():
+        line = line.rstrip()    # Removal of trailing '\n'
+        line = line.lstrip(">") # Removal of trailing '>' quotation symbol
+        data += line + " "
+    data = data.lower() # To lowercase
 
-# Stopwords removal
-stop_words = stopwords.words('english')
-data_ = ""
-for word in data.split():
-    if word not in stop_words:
-        data_ = data_ + " " + word
+    # Stopwords removal
+    stop_words = stopwords.words('english')
+    data_ = ""
+    for word in data.split():
+        if word not in stop_words:
+            data_ = data_ + " " + word
 
-# Stemmer
-snow_stemmer = SnowballStemmer(language='english')
-stem_words = ""
-for w in data_.split():
-    x = snow_stemmer.stem(w)
-    stem_words = stem_words + " " + x
+    # Stemmer
+    snow_stemmer = SnowballStemmer(language='english')
+    stem_words = ""
+    for w in data_.split():
+        x = snow_stemmer.stem(w)
+        stem_words = stem_words + " " + x
 
-print(stem_words)
+    return stem_words
 
